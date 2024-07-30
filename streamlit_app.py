f888b75b9aacc20c41465b9f058e35d8d4cdf222
Ahmed Mohamed <AhmedMohamed241998@gmail.com>
2024-07-30 19:13:52 +0300

    Update Time

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
 
 # Display Title and Description
@@ -11,7 +11,7 @@ st.title("L'Oréal Action Plan")
 conn = st.experimental_connection('gsheets',type=GSheetsConnection)
 
 # Ftech Exisiting Vendors data
-existing_data = conn.read(worksheet= 'Data',usecols = list(range(5)) , ttl = 5)
+existing_data = conn.read(worksheet= 'Operators Data',usecols = list(range(5)) , ttl = 5)
 existing_data = existing_data.dropna(how="all")
 
 # List of Machine Names
@@ -20,16 +20,18 @@ machine_name = [
     "LSH02",
     "LSH06",
     "LSH04",
-    "LSH05"
+    "LSH05",
+    "All Machines"
 ]
 # Onboarding New Vendor Form
 with st.form(key="action_plan_form"):
-    date = datetime.now()
+    timezone_offset = 3  # Pacific Standard Time (UTC−08:00)
+    tzinfo = timezone(timedelta(hours=timezone_offset))
+    date = datetime.now(tzinfo)
     name = st.text_input(label="Name")
-    machine_name = st.selectbox("Machine", machine_name)
+    machine_name = st.selectbox("Machine", machine_name,default = "Select Machine")
     problem = st.text_area(label="Problem Details")
     action = st.text_area(label="Action Details")
-
     submit_button = st.form_submit_button(label="Submit")
 
     # If the submit button is pressed
@@ -51,8 +53,8 @@ with st.form(key="action_plan_form"):
                     }
                 ]
             )
-
             # Add the new vendor data to the existing data
             updated_df = pd.concat([existing_data, action_data], ignore_index=True)
-            conn.update(worksheet='Data',data=updated_df)
-!pipreqs ./
+            conn.update(worksheet='Operators Data',data=updated_df)
+            st.success("Action is submitted")
+
